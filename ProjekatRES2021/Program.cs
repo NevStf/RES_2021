e162551602d16a8bab764ee311d471d8110199e3
Nevena Stefanović <Nevena_98_Cuprija@hotmail.com>
2021-04-18 16:47:06 +0200

    Add project files.

---
+++ ProjekatRES2021/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatRES2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("evil troy and evil abeeed");
+            Console.ReadKey();
+        }
+    }
+}
