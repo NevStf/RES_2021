5b0003596926239f76735ef4892234eb2a13df7d
BranaYo <AS@DESKTOP-HVEUJEO>
2021-06-06 23:08:54 +0200

    Serijalizacija klase description

--- Contracts/Resources/Description.cs
+++ Contracts/Resources/Description.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Contracts.Resources
 {
+    [DataContract]
     public class Description
     {
+        [DataMember]
         public int ID { get; set; }
+        [DataMember]
         public List<Item> Items { get; set; }
+        [DataMember]
         public int DataSet { get; set; }
 
         public Description(int id, int ds)
